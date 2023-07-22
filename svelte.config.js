4eece76ec60e34d291bfc16d5ea1d9466805455b
samyaknahta2002 <97379283+samyaknahta2002@users.noreply.github.com>
2023-07-22 14:58:29 +0530

    project

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-aut';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
